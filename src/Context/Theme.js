cab898852767b6df53cb3766b82e5abfccd0e702
jahidulislamzim <jahidulislamzim845@gmail.com>
2022-12-27 17:10:26 +0600

    adding readme

--- src/Context/Theme.js
+++ src/Context/Theme.js
@@ -1,26 +1,38 @@
 import { createContext, useEffect, useState } from 'react'
+import { theme } from '../Data/Data';
 
 
 const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
+
+  
   const [themeName, setThemeName] = useState('light');
   const [updateState, setUpdateState] = useState(true)
 
   useEffect(() => {
-
+    var root =  document.documentElement; 
     const data = localStorage.getItem('themeName');
     setThemeName(data)
     if(themeName === 'light'){
-        window.document.body.classList.add(themeName);
-        window.document.body.classList.remove('dark');
+      for(let property in theme.light) {		
+        let key = property;
+        let value = theme.light[property];
+        root.style.setProperty(key, value)
+      }
     } else{
-        window.document.body.classList.add(themeName);
-        window.document.body.classList.remove('light');
+        for(let property in theme.dark) {		
+          let key = property;
+          let value = theme.dark[property];
+          root.style.setProperty(key, value)
+        }
     }
 
   }, [updateState, themeName])
 
+  
+
+  // console.log(theme.dark.key)
   
 
   const toggleTheme = (e) => {
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,172 +1,184 @@
 const header = {
-    title:'developerInfo',
-    titleLink:'https://github.com/jahidulislamzim'
-}
-
+  title: "developerInfo",
+  titleLink: "https://github.com/jahidulislamzim",
+};
 
 const myInfo = {
-    name:'Jahidul Islam Zim',
-    titles:[
-        // add your title. you can add more 
-        'MERN Stack Developer',
-        'FrontEnd Engineer'
-    ],
+  name: "Jahidul Islam Zim",
+  titles: [
+    // add your title. you can add more
+    "MERN Stack Developer",
+    "FrontEnd Engineer",
+  ],
 
-    SocialLinks:{
-        // add only your social media user names do not add more it will not work properly.
-        linkedIn:'jahidulislamzim',
-        gitHub:'jahidulislamzim',
-        facebook:'jahidulislamzim43',
-        twitter:'jahidul_zim',
-        behance:'jahidul-islam',
-        dribbble:'jahidul-islam',
-        instagram: 'jahidulislamzim'
-    },
-    // add your image name with image extention.
-    // example zim.jpeg, zim.png, zim.svg etc.
-    image:'zim.webp',
-    //add your Resume name with file extention.
-    resume:'zim.pdf',
-    // add your bio here within 590 characters
+  SocialLinks: {
+    // add only your social media user names do not add more it will not work properly.
+    linkedIn: "jahidulislamzim",
+    gitHub: "jahidulislamzim",
+    facebook: "jahidulislamzim43",
+    twitter: "jahidul_zim",
+    behance: "jahidul-islam",
+    dribbble: "jahidul-islam",
+    instagram: "jahidulislamzim",
+  },
+  // add your image name with image extention.
+  // example zim.jpeg, zim.png, zim.svg etc.
+  image: "zim.webp",
+  //add your Resume name with file extention.
+  resume: "zim.pdf",
+  // add your bio here within 590 characters
 
-    bio:'An aspiring MERN Stack Developer.I lost myself when I dive into the codes. I am a professional web designer and developer.No project is too big or too small for me. Recently I have completed react and node js. I am expertise in Html5, Css3, Javascript, Bootstrap, Jquery, React JS, Next JS, Node JS, Express JS, Mongo DB.Resourceful Web Developer lends hands-on approach to customizing web presence strategy. More than 1+ years of experience working closely with Web Development to outline organizational needs and translate them into extensive lines of code that support objectives. ',
+  bio: "An aspiring MERN Stack Developer.I lost myself when I dive into the codes. I am a professional web designer and developer.No project is too big or too small for me. Recently I have completed react and node js. I am expertise in Html5, Css3, Javascript, Bootstrap, Jquery, React JS, Next JS, Node JS, Express JS, Mongo DB.Resourceful Web Developer lends hands-on approach to customizing web presence strategy. More than 1+ years of experience working closely with Web Development to outline organizational needs and translate them into extensive lines of code that support objectives. ",
 
-    age:'19+',
-    position: 'FrontEnd Engineer',
-    qualification:'Web Programming-CS50',
-    languages:'bangla and english',
-    experience:'1 year',
-    completedProject:'10+',
-    happyClients:'10+',
-    awardsWon:'1'
-}
-
+  age: "19+",
+  position: "FrontEnd Engineer",
+  qualification: "Web Programming-CS50",
+  languages: "bangla and english",
+  experience: "1 year",
+  completedProject: "10+",
+  happyClients: "10+",
+  awardsWon: "1",
+};
 
 const skills = [
-    {
-        title:'HTML',
-        level:'Expart'
-    },
-    {
-        title:'CSS',
-        level:'Expart'
-    },
-    {
-        title:'Bootstrap',
-        level:'Expart'
-    },
-    {
-        title:'Tailwind CSS',
-        level:'Expart'
-    },
-    {
-        title:'JavaScript',
-        level:'Expart'
-    },
-    {
-        title:'React JS',
-        level:'Expart'
-    },
-    {
-        title:'Next JS',
-        level:'Mid Level'
-    },
-    {
-        title:'Meterial UI',
-        level:'Expart'
-    },
-    {
-        title:'GitHub',
-        level:'Expart'
-    },
-    {
-        title:'FireBase',
-        level:'Mid Level'
-    },
-    {
-        title:'Express JS',
-        level:'Mid Level'
-    },
-    {
-        title:'Node JS',
-        level:'Mid Level'
-    },
-    {
-        title:'MongoDB',
-        level:'junior'
-    },
-]
-
+  {
+    title: "HTML",
+    level: "Expart",
+  },
+  {
+    title: "CSS",
+    level: "Expart",
+  },
+  {
+    title: "Bootstrap",
+    level: "Expart",
+  },
+  {
+    title: "Tailwind CSS",
+    level: "Expart",
+  },
+  {
+    title: "JavaScript",
+    level: "Expart",
+  },
+  {
+    title: "React JS",
+    level: "Expart",
+  },
+  {
+    title: "Next JS",
+    level: "Mid Level",
+  },
+  {
+    title: "Meterial UI",
+    level: "Expart",
+  },
+  {
+    title: "GitHub",
+    level: "Expart",
+  },
+  {
+    title: "FireBase",
+    level: "Mid Level",
+  },
+  {
+    title: "Express JS",
+    level: "Mid Level",
+  },
+  {
+    title: "Node JS",
+    level: "Mid Level",
+  },
+  {
+    title: "MongoDB",
+    level: "junior",
+  },
+];
 
 const projects = [
-    {
-        title:'project-01',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject01.webp'
-    },
-    {
-        title:'project-02',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject02.webp'
-    },
-    {
-        title:'project-03',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject03.webp'
-    },
-    {
-        title:'project-04',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject04.webp'
-    },
-    {
-        title:'project-05',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject05.webp'
-    },
-    {
-        title:'project-06',
-        details:`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`, 
-        source:'https://github.com/jahidulislamzim',
-        live:'https://jahidulislamzim.netlify.app/',
-        image:'DemoProject06.webp'
-    },
-    
-]
-
+  {
+    title: "project-01",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject01.webp",
+  },
+  {
+    title: "project-02",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject02.webp",
+  },
+  {
+    title: "project-03",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject03.webp",
+  },
+  {
+    title: "project-04",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject04.webp",
+  },
+  {
+    title: "project-05",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject05.webp",
+  },
+  {
+    title: "project-06",
+    details: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer to.`,
+    source: "https://github.com/jahidulislamzim",
+    live: "https://jahidulislamzim.netlify.app/",
+    image: "DemoProject06.webp",
+  },
+];
 
 const contact = {
-    phone:'+8801780115943',
-    email:'jahidulislamzim845@gmail.com',
-    location:'rangpur, Kurigram, Bangladesh',
-    serviceId:'service_pus8j8k',
-    templateId:'template_v709mo3',
-    publicKey: '1cSlHB3iMQ-aGi_J0'
-}
-
+  phone: "+8801780115943",
+  email: "jahidulislamzim845@gmail.com",
+  location: "rangpur, Kurigram, Bangladesh",
+  serviceId: "service_pus8j8k",
+  templateId: "template_v709mo3",
+  publicKey: "1cSlHB3iMQ-aGi_J0",
+};
 
 const seo = {
-    title:'Jahidul Islam',
-    favicon:'zim.pn',
-    keywords:'jahidul islam zim',
-    url:'jahidulislamzim.netlify.com',
-    description:`seo section is awasome`,
+  title: "Jahidul Islam",
+  favicon: "zim.pn",
+  keywords: "jahidul islam zim",
+  url: "jahidulislamzim.netlify.com",
+  description: `seo section is awasome`,
+};
 
-}
+const theme = {
+  light: {
+    "--primary": "#19bf70",
+    "--background": "#fcfcfc",
+    "--foreground": "#444",
+    "--text": "#fff",
+    "--text-alt": "#24da8e",
+    "--shadow": "#64646f33",
+    "--error": "#E1306C",
+    "--transition": "all 400ms ease",
+  },
 
+  dark: {
+    "--primary": "#19bf70",
+    "--background": "#23283e",
+    "--foreground": "#fcfcfc",
+    "--text": "#fff",
+    "--text-alt": "#24da8e",
+    "--shadow": "#d3d2d22c",
+    "--error": "#E1306C",
+    "--transition": "all 400ms ease",
+  },
+};
 
-
-
-
-
-export {header, myInfo, skills, projects, contact, seo};
+export { header, myInfo, skills, projects, contact, seo, theme };
